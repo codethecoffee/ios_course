7fb1c3934d3151d689554d211006d2f2c26708c3
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-19 13:42:51 +0900

    Fix bug where updateTimer is called indefinitely even after countdown is finished

--- section-8/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ section-8/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["soft": 3, "medium": 4, "hard": 7]
-    var secondsLeft = 0
+    
+    let eggTimes: [String: Float] = ["soft": 3.0, "medium": 4.0, "hard": 7.0]
+    var secondsLeft: Float = 0
+    var currHardness: String = ""
+    var totalTime: Float = 0
+    
+    // Declare an instance of Timer() so you can access it
+    // in other functions like updateTimer()
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!.lowercased()
+        currHardness = sender.currentTitle!.lowercased()
         
-        secondsLeft = eggTimes[hardness]!
+        totalTime = eggTimes[currHardness]!
+        secondsLeft = eggTimes[currHardness]!
         
         // selector: Function called for every timeInterval (in this case, every second)
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
     }
     
@@ -30,10 +39,15 @@ class ViewController: UIViewController {
         if secondsLeft > 0 {
             print("\(secondsLeft) seconds left")
             secondsLeft -= 1
+            progressBar.progress = (totalTime - secondsLeft) / totalTime
         }
         else {
             print("Done")
             countdownLabel.text! = "Done!"
+            
+            // Invalidate method prevents updateTimer()
+            // from being called indefinitely
+            timer.invalidate()
         }
     }
     
