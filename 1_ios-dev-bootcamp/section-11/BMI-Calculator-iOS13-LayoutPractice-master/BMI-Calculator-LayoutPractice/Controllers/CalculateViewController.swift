219891665019d386bb25d3e625db470dd638f6fb
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-22 13:31:08 +0900

    Complete recalculate functionality

--- 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/CalculateViewController.swift
+++ 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/CalculateViewController.swift
@@ -17,18 +17,17 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
     
+    var bmi: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         print("Current height: " + String(format: "%.1f", sender.value))
         heightLabel.text = String(format: "%.1f", sender.value)
     }
-    
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         print("Current weight: \(Int(sender.value))")
@@ -38,13 +37,33 @@ class CalculateViewController: UIViewController {
         
         let height = heightSlider.value
         let weight = weightSlider.value
-        let bmi: Float = weight / pow(height, 2)
+        bmi = weight / pow(height, 2)
         print(bmi)
         
         // Sender: The viewController initiating the transition
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
+    
+    /**
+     Function triggered whenever self.performSegue is called.
+     This same function is called even if there are multiple different segues.
 
+     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Make sure you are using the correct segue
+        if segue.identifier == "goToResult" {
+            
+            // Downcast to ResultViewController in order to access
+            // the property bmi that is unique to that subclass
+            // The superclass UIViewController does not have it, thus
+            // the code will not run if you don't downcast
+            let destVC = segue.destination as! ResultViewController
+            destVC.bmi = self.bmi
+        }
+        
+        
+    }
     
 
 }
--- 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
+++ 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
@@ -9,26 +9,23 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var bmi: Float = 0.0
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bmiLabel.text = String(format: "%.1f", bmi)
 
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
         
+        // Dismiss the second view upon pressing recalculate
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
