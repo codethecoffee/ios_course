2ad2712f9c1e126e73dba38314488827946c5b68
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-22 17:18:06 +0900

    Complete fully functional tip calculator

--- 1_ios-dev-bootcamp/section-11...15/section-12/Tipsy-iOS13-master/Tipsy/Controller/CalculatorViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-12/Tipsy-iOS13-master/Tipsy/Controller/CalculatorViewController.swift
@@ -17,7 +17,9 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     var selectedTip: Float = 0.1
-    var splitNum: Int = 2
+    var splitNum: Float = 2
+    var billTotal: Float = 0.0
+    var totalPerPerson: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,9 @@ class CalculatorViewController: UIViewController {
         UIUpdate()
         sender.isSelected = true
         
+        // Dismisses the keyboard if it's open
+        billTextField.endEditing(true)
+        
     }
     
     func UIUpdate() {
@@ -44,13 +49,40 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func splitValueChanged(_ sender: UIStepper) {
         splitNumberLabel.text = String(Int(sender.value))
-        self.splitNum = Int(sender.value)
+        self.splitNum = Float(sender.value)
         
+        // Dismisses the keyboard if it's open
+        billTextField.endEditing(true)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print("Selected tip: \(self.selectedTip)")
-        print("Split amongst: \(self.splitNum)")
+        
+        let billNum = Float(billTextField.text!)
+        
+        let unsplitTotal = (billNum ?? 0.0) + selectedTip*(billNum ?? 0.0)
+        self.totalPerPerson = unsplitTotal / splitNum
+        
+        // Transition to Results view on click
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+
+    }
+    
+    /**
+     Function triggered whenever self.performSegue is called.
+     This same function is called even if there are multiple different segues.
+
+     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Make sure you are using the correct segue
+        if segue.identifier == "goToResults" {
+            let resultsVC = segue.destination as! ResultsViewController
+            resultsVC.totalPerPerson = self.totalPerPerson
+            resultsVC.tip = self.selectedTip
+            resultsVC.numSplit = self.splitNum
+        }
+        
+        
     }
 }
 
--- 1_ios-dev-bootcamp/section-11...15/section-12/Tipsy-iOS13-master/Tipsy/Controller/ResultsViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-12/Tipsy-iOS13-master/Tipsy/Controller/ResultsViewController.swift
@@ -11,24 +11,22 @@ import UIKit
 class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    var totalPerPerson: Float = 0.0
+    var numSplit: Float = 2
+    var tip: Float = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        totalLabel.text = String(self.totalPerPerson)
+        subtitleLabel.text = "Split between " + String(numSplit) + "people, with " + String(tip) + "tip"
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
