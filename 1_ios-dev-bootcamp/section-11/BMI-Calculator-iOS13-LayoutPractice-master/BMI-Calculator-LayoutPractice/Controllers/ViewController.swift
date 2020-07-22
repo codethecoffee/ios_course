b0c951601955410a1a0eba322ab9219e7d2f34b9
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-22 12:04:45 +0900

    Implement functional sliders

--- 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ 1_ios-dev-bootcamp/section-11/BMI-Calculator-iOS13-LayoutPractice-master/BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print("Current height: " + String(format: "%.1f", sender.value))
+        heightLabel.text = String(format: "%.1f", sender.value)
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print("Current weight: \(Int(sender.value))")
+        weightLabel.text = String(Int(sender.value))
+    }
+    
 
 }
 
