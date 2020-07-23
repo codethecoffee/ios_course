dce7536004a71517cbaf68d3c431b66aac258f5c
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-23 17:17:48 +0900

    Add a swift file showing why relying on class inheritance can get messy

--- 1_ios-dev-bootcamp/section-11...15/section-13/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-13/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     // Can use icons from SF Symbols for iOS13+
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // The text field reports back to WeatherViewController
+        searchTextField.delegate = self
     }
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text ?? "No city entered")
+        
+        // Dismisses the keyboard upon pressing search button
+        searchTextField.endEditing(true)
+        
+    }
+    
+    /**
+     Function that is fired when the the return button on the keyboard is pressed
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text ?? "No city entered")
+        
+        // Dismisses the keyboard upon pressing return
+        searchTextField.endEditing(true)
 
+        // Return true if the keyboard should indeed return upon press
+        return true
+    }
+    
+    /**
+     Function triggered when user tries to deselect the text field. Useful for doing validation on what the user typed
+     */
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        // Valid input
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Invalid city name"
+            // Returns false, so we don't proceed further
+            return false
+        }
+    }
+    
+    
+    /**
+     Function triggered when user is done editing a UI Text field
+     */
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        // This clears the search bar once you search
+        searchTextField.text = ""
+    }
+    
+
+    
 
 }
 
---
+++ 1_ios-dev-bootcamp/section-11...15/section-13/Protocols Demo/Protocols Demo/main.swift
@@ -0,0 +1,87 @@
+//
+//  main.swift
+//  Protocols Demo
+//
+//  Created by Suzy Lee on 7/23/20.
+//  Copyright © 2020 Suzy Lee. All rights reserved.
+//
+
+/**
+ A swift code file showing why we need protocols. Relying on class inheritance can get very messy.
+ 
+ The premise: We own a flying museum, where we demo flying birds. We, quite reasonably, start off by
+ declaring a Bird class. All Birds can lay eggs (if female) and fly. We start creating subclasses for more
+ specific kinds of birds, and it works great so far for the Eagle.
+ 
+ However, now we're going to show penguins as well (don't question it), and now penguins can fly because
+ they're inheriting from the class Bird!
+ 
+ Our flying demos are a huge success so we decide to show demos of flying objects as well. The flyingDemo
+ method only takes Bird objects, thus we're forced to define our Plane class as a subclass of Bird. Now,
+ because of this, planes can lay eggs! What a mess.
+ 
+ */
+
+import Foundation
+
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird!")
+        }
+    }
+    
+    func fly() {
+        print("The bird flaps its wings and lifts off into the sky.")
+    }
+}
+
+class Eagle: Bird {
+    
+    func soar() {
+        print("The eagle glides in the air. Not all birds can do this.")
+    }
+    
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the waater. Only penguins can do this.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: Bird) {
+        flyingObject.fly()
+    }
+}
+
+// We want to now show airplanes in the flying demo!
+class Airplane: Bird {
+    override func fly() {
+        print("The airplane uses its engine to lift into the air.")
+    }
+}
+
+
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+
+let myPenguin = Penguin()
+myPenguin.layEgg()
+myPenguin.swim()
+// We want penguin to be a subtype of Bird, but we don't want it to be able to fly.
+myPenguin.fly()
+
+
+let myPlane = Airplane()
+
+let museum = FlyingMuseum()
+
+// Because a penguin is a Bird object, you can also pass in a myPenguin object!
+// We're relying TOO MUCH on class inheritance.
+museum.flyingDemo(flyingObject: myPenguin)
