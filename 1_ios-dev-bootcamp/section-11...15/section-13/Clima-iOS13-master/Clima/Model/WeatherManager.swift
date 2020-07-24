36bb8221797b736fee7f7dc1c538917e40dde8df
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-24 17:17:19 +0900

    Refactor completionHandler so it is a closure

--- 1_ios-dev-bootcamp/section-11...15/section-13/Clima-iOS13-master/Clima/Model/WeatherManager.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-13/Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -30,7 +30,18 @@ struct WeatherManager {
             // There are multiple different kinds of dataTask methods.
             // This one retrieves the contents from the url, and then fires a completionHandler
             // after successfully fetching said data.
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    // Data objects aren't as printable as strings; need to convert
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
             
             //4. Start the task to trigger the entire networking process
             task.resume()
@@ -38,21 +49,22 @@ struct WeatherManager {
     }
     
     
-    // Note: It's better programming practice to pass in a closure as the completionHandler instead
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        // If there's an error, print it and exit out of the function
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            // Data objects aren't as printable as strings; need to convert
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
-        }
-        
-    }
+    // Note: It's better programming practice to pass in a closure as the completionHandler instead.
+    // That's why I commented this out.
+    //    func handle(data: Data?, response: URLResponse?, error: Error?) {
+    //        // If there's an error, print it and exit out of the function
+    //        if error != nil {
+    //            print(error!)
+    //            return
+    //        }
+    //
+    //        if let safeData = data {
+    //            // Data objects aren't as printable as strings; need to convert
+    //            let dataString = String(data: safeData, encoding: .utf8)
+    //            print(dataString!)
+    //        }
+    //
+    //    }
     
     
 }
