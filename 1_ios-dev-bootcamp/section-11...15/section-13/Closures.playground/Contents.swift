c0b17c1bbc1fb70335da028348b032098b53df75
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-24 17:08:53 +0900

    Add two more examples of how to use closures (more practical examples this time)

--- 1_ios-dev-bootcamp/section-11...15/section-13/Closures.playground/Contents.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-13/Closures.playground/Contents.swift
@@ -1,11 +1,14 @@
 import UIKit
 
+/**
+ EXAMPLE 1: CALCULATOR
+ */
+
 func calculator(num1: Int, num2: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(num1, num2)
 }
 
 // Let's shorten our multiply function as much as possible using CLOSURES!"
-
 // Original version
 func multiply (num1: Int, num2: Int) -> Int {
     return num1 * num2
@@ -30,3 +33,39 @@ calculator(num1: 2, num2: 3) {$0 * $1}
 
 // Note: To somebody who doesn't know Swift very well, version 5 probably looks like a bunch
 // of nonsense.
+
+
+
+
+
+/**
+ EXAMPLE 2: Performing a simple operation on every element in an array
+ */
+
+// TASK: We want to add one to every element in the array below
+let nums = [6, 2, 3, 9, 4, 1]
+
+// Version 1: Define an addOne function and map it over each element in the array
+func addOne(n1: Int) -> Int {
+    return n1 + 1
+}
+print(nums.map(addOne))
+
+// Version 2: Use a closure so you don't have to define addOne separately
+print(nums.map({(num1) in return num1+1}))
+
+// Version 3: Use anonymous parameters and remove the return statement
+print(nums.map({$0+1}))
+
+// Version 4: Make it trailing
+print(nums.map{$0+1})
+
+
+
+
+
+/**
+ EXAMPLE 3: Concisely stringifying an array
+ */
+let stringifiedNums = nums.map{"\($0)"}
+print(stringifiedNums)
