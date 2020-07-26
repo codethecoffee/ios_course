3a3ba7ea9572d5c03faccdf5fd1509c35ff3711c
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-26 17:23:19 +0900

    Implement typing animation using the CLTypingLabel Pod

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,26 +7,17 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-    
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
+        titleLabel.text = "⚡️FlashChat"
+        titleLabel.charInterval = 0.3
         
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            // The timers for each letter start at the same time
-            // Thus, we need to multiply by char Index to scale
-            // the time interval accordingly
-            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        
     }
 }
