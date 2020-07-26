6d2440347e4f3144f5390966466468ba2c71a191
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-26 16:33:44 +0900

    Add typing animation using scheduled timers

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            // The timers for each letter start at the same time
+            // Thus, we need to multiply by char Index to scale
+            // the time interval accordingly
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-    
 }
