602051e2053c9669f6aef8bfbe2cf69ddf58cd51
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-26 18:51:56 +0900

    Implemented new user registration with firebase

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -15,13 +15,19 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "registerToChat", sender: self)
-        
         // OPTIONAL CHAINING
         // Now the Auth.auth().createUser method is called only if both the email and password
         // variables are not nil after unpacking.
         if let email = emailTextfield.text, let password = passwordTextfield.text {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                // If there was an error (you can also give user feedback in a pop up
+                if let e = error {
+                    // TODO: Add a pop up that displays this localized description
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to the chat view controller
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                }
             }
         }
     }
