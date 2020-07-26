14fb7dbdadc2c9909acaacf85bbe78233d5398b0
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-26 18:41:36 +0900

    Use optional chaining to nest Auth method

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "registerToChat", sender: self)
+        
+        // OPTIONAL CHAINING
+        // Now the Auth.auth().createUser method is called only if both the email and password
+        // variables are not nil after unpacking.
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            }
+        }
     }
- 
     
 }
