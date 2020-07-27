2de61a7a5bee36481bf55bdd4a1282681f554b3f
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-27 17:21:34 +0900

    Implement full login functionality

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,10 +1,12 @@
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,6 +15,12 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+//          // ...
+//        }
+//    }
     
 
 }
--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    
     @IBAction func loginPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "loginToChat", sender: self)
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                    return
+                } else {
+                    self.performSegue(withIdentifier: "loginToChat", sender: self)
+                }
+            }
+            
+        }
+        
     }
     
 }
