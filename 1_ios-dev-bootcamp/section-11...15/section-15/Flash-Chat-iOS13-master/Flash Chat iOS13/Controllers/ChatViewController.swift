f0500a880119a565dac1c61aad4ad1b4e41300fe
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-28 15:03:55 +0900

    Use popToRootViewController to navigate to welcome screen on logout

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -3,24 +3,32 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-//          // ...
-//        }
-//    }
+    /**
+     Logging out the user
+     */
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            // Takes user back to the root VC (welcome view)
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
     
-
+    
 }
