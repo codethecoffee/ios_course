d87bf418444d69dfd169e49e71dc1c29dda71490
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-07-28 15:31:43 +0900

    Starting to organize the constants struct

--- 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Constants.swift
+++ 1_ios-dev-bootcamp/section-11...15/section-15/Flash-Chat-iOS13-master/Flash Chat iOS13/Constants.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2020 Angela Yu. All rights reserved.
 //
 
-import Foundation
+
+struct Constants {
+    // Changes this from an INSTANCE property to a TYPE property
+    // This is a property associated with the Constants data type
+    
+    // Why use static keywords?
+    // You no longer have to create an instance of the Constants struct
+    // in order to access the value of a type property.
+    static let registerToChatSegue = "registerToChat"
+    static let loginToChatSegue = "loginToChat"
+}
